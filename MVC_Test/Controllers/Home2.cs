30ddd22976e522851f638ca368174355fa82f644
Jeff <jeff@Jeffs-MacBook-Pro-2.local>
2024-06-13 22:04:50 +0800

    1st version, a simple understanding MVC structure.

---
+++ MVC_Test/Controllers/Home2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC_Test.Controllers
+{
+    public class ERP : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
+
