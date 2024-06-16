1aa6c6c4343f1fbc4475673723fe0430c14de954
Jeff <jeff@Jeffs-MacBook-Pro-2.local>
2024-06-16 18:44:17 +0800

    On desktop
    Manually delete and update folders & files.

---
+++ MVC_Test/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MVC_Test.Models;
+
+public class LoginViewModel
+{
+    public string? AccountID { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? EMail { get; set; }
+
+    public string? Country { get; set; }
+
+    public string? Address { get; set; }
+}
